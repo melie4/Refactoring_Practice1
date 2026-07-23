418d755060f79940c2e71b8635ded6f22caac026
melie4 <hanameli@m04.itscom.net>
2026-07-23 21:32:17 +0900

    メソッド名を簡潔に

--- MethodNaming/AssignmentTask.java
+++ MethodNaming/AssignmentTask.java
@@ -11,7 +11,7 @@ public class AssignmentTask {
   /*
    * パターン2
    */
-  public void getUserMessageAndPrintItToTheConsoleWithHelloPrefixAndUserName(User user) {
+  public void getUserMessage(User user) {
     System.out.println("Hello, " + user.getName());
   }
 
